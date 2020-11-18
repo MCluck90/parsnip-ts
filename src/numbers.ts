03b7f312e6bd7711eda6423328c6b928fc51b856
Mike Cluck <mike-cluck@pluralsight.com>
2020-11-18 09:47:49 -0700

    Add floating point parsers

--- src/numbers.ts
+++ src/numbers.ts
@@ -1,9 +1,27 @@
-import { regexp } from './parser';
+import { maybeWithDefault, regexp, text } from './parser';
 
-export const digit = regexp(/[0-9]/y);
+export const digit = regexp(/\d/y);
 
-export const integer = regexp(/0|[1-9][0-9]+/y).map(Number);
+export const integer = regexp(/0|[1-9][0-9]*/y).map(Number);
 
-export const separatedInteger = regexp(/0|[1-9][0-9_]+/y).map((d) =>
+export const separatedInteger = regexp(/0|[1-9][0-9_]*/y).map((d) =>
   Number(d.replace(/_/g, ''))
 );
+
+export const sign = regexp(/[+-]/y);
+
+export const exponent = regexp(/[eE]/y)
+  .concat(maybeWithDefault(sign, ''))
+  .concat(regexp(/\d+/y));
+
+export const fraction = text('.').concat(maybeWithDefault(regexp(/\d+/y), ''));
+
+export const floatingPoint = integer
+  .concat(fraction)
+  .concat(maybeWithDefault(exponent, ''))
+  .map(Number);
+
+export const separatedFloatingPoint = separatedInteger
+  .concat(fraction)
+  .concat(maybeWithDefault(exponent, ''))
+  .map(Number);
--- tests/numbers.test.ts
+++ tests/numbers.test.ts
@@ -1,5 +1,14 @@
 import { oneOrMore } from '../src/lib';
-import { digit, integer, separatedInteger } from '../src/numbers';
+import {
+  digit,
+  exponent,
+  floatingPoint,
+  fraction,
+  integer,
+  separatedFloatingPoint,
+  separatedInteger,
+  sign,
+} from '../src/numbers';
 import { assertSuccessfulParse, assertUnsuccessfulParse } from './util';
 
 describe('digit', () => {
@@ -34,5 +43,92 @@ describe('separatedInteger', () => {
     const result = separatedInteger.parseStringToCompletion('1_234_567_890');
     assertSuccessfulParse(result);
     expect(result).toBe(1234567890);
+  });
+});
+
+describe('sign', () => {
+  test('matches a negative sign', () => {
+    const result = sign.parseStringToCompletion('-');
+    assertSuccessfulParse(result);
+    expect(result).toBe('-');
+  });
+
+  test('matches a positive sign', () => {
+    const result = sign.parseStringToCompletion('+');
+    assertSuccessfulParse(result);
+    expect(result).toBe('+');
+  });
+});
+
+describe('exponent', () => {
+  test('matches a simple lowercase exponent', () => {
+    const result = exponent.parseStringToCompletion('e10');
+    assertSuccessfulParse(result);
+  });
+
+  test('matches a simple uppercase exponent', () => {
+    const result = exponent.parseStringToCompletion('E10');
+    assertSuccessfulParse(result);
+  });
+
+  test('matches a signed exponent', () => {
+    assertSuccessfulParse(exponent.parseStringToCompletion('e-10'));
+    assertSuccessfulParse(exponent.parseStringToCompletion('e+10'));
+  });
+});
+
+describe('fraction', () => {
+  test('matches a single dot', () => {
+    assertSuccessfulParse(fraction.parseStringToCompletion('.'));
+  });
+
+  test('matches a dot followed by digits', () => {
+    assertSuccessfulParse(fraction.parseStringToCompletion('.12345678790'));
+  });
+});
+
+describe('floatingPoint', () => {
+  test('matches a simple floating point', () => {
+    const result = floatingPoint.parseStringToCompletion('1.');
+    assertSuccessfulParse(result);
+    expect(result).toBe(1);
+  });
+
+  test('matches a floating point', () => {
+    const result = floatingPoint.parseStringToCompletion('1.2345');
+    assertSuccessfulParse(result);
+    expect(result).toBe(1.2345);
+  });
+
+  test('matches a floating point with an exponent', () => {
+    const result = floatingPoint.parseStringToCompletion('1.2345e6');
+    assertSuccessfulParse(result);
+    expect(result).toBe(1.2345e6);
+  });
+});
+
+describe('separatedFloatingPoint', () => {
+  test('matches a simple floating point', () => {
+    const result = separatedFloatingPoint.parseStringToCompletion('1.');
+    assertSuccessfulParse(result);
+    expect(result).toBe(1);
+  });
+
+  test('matches a floating point', () => {
+    const result = separatedFloatingPoint.parseStringToCompletion('1.2345');
+    assertSuccessfulParse(result);
+    expect(result).toBe(1.2345);
+  });
+
+  test('matches a floating point with an exponent', () => {
+    const result = separatedFloatingPoint.parseStringToCompletion('1.2345e6');
+    assertSuccessfulParse(result);
+    expect(result).toBe(1.2345e6);
+  });
+
+  test('allows underscores in the integer', () => {
+    const result = separatedFloatingPoint.parseStringToCompletion('1_000.2e3');
+    assertSuccessfulParse(result);
+    expect(result).toBe(1000.2e3);
   });
 });
